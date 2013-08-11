1d527e35bd9949470443332045bb61c1259b9ee4
Charles Kerr <charles.kerr@canonical.com>
2013-08-11 11:56:33 -0500

    add compiler warnings, gettext boilerplate

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <glib/gi18n.h>
-#include <glib.h>
-
-int
-main (int argc, char * argv[])
-{
-  g_message ("Hello, world!");
-  return 0;
-}
---
+++ src/main.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * Authors:
+ *   Charles Kerr <charles.kerr@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <locale.h>
+
+#include <QtGlobal>
+
+#include <glib/gi18n.h>
+#include <glib.h>
+
+/***
+****
+***/
+
+int
+main (int argc, char ** argv)
+{
+  Q_UNUSED (argc);
+  Q_UNUSED (argv);
+
+  /* boilerplate i18n */
+  setlocale (LC_ALL, "");
+  bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+  textdomain (GETTEXT_PACKAGE);
+
+  g_message ("Hello, world!");
+  return 0;
+}
